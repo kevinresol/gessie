e1150f14e6a2595c485820a9cdf03cdaa2a68e95
Kevin Leung <kevinresol@gmail.com>
2015-10-31 21:14:18 +0800

    Fix luxe input adapter

--- src/gessie/impl/luxe/LuxeInputAdapter.hx
+++ src/gessie/impl/luxe/LuxeInputAdapter.hx
@@ -4,6 +4,7 @@ import gessie.core.IInputAdapter;
 import gessie.core.TouchManager;
 import luxe.Input.MouseEvent;
 import luxe.Input.TouchEvent;
+import luxe.Vector;
 import luxe.Visual;
 
 using luxe.utils.GeometryUtils;
@@ -19,21 +20,26 @@ class LuxeInputAdapter implements IInputAdapter<Visual>
 	
 	@:isVar
 	public var touchManager(get, set):TouchManager<Visual>;
+	
+	var helperVector:Vector;
 
 	public function new() 
 	{
-		
+		helperVector = new Vector();
 	}
 	
 	public function init():Void
 	{
 		Luxe.on(Luxe.Ev.update, function(_) Gessie.update());
+		#if mobile
 		Luxe.on(Luxe.Ev.touchdown, ontouchdown);
 		Luxe.on(Luxe.Ev.touchup, ontouchup);
 		Luxe.on(Luxe.Ev.touchmove, ontouchmove);
+		#else
 		Luxe.on(Luxe.Ev.mousedown, onmousedown);
 		Luxe.on(Luxe.Ev.mouseup, onmouseup);
 		Luxe.on(Luxe.Ev.mousemove, onmousemove);
+		#end
 	}
 	
 	function onmousedown(e:MouseEvent)
@@ -75,29 +81,39 @@ class LuxeInputAdapter implements IInputAdapter<Visual>
 	{
 		var target = null; // = getVisualUnderPoint();
 		var depth = -1.;
-		
-		for (e in Luxe.scene.entities)
+		var pos = Luxe.camera.screen_point_to_world(translateTouchPos(e.pos,helperVector));
+		for(en in LuxeDisplayListAdapter.targets)
 		{
-			var v = Std.instance(e, Visual);
-			if (v != null && v.depth > depth && Luxe.utils.geometry.point_in_geometry(e.pos, v.geometry) )
+			var v = Std.instance(en, Visual);
+			if (v != null && v.depth >= depth && Luxe.utils.geometry.point_in_geometry(pos, v.geometry))
 			{
 				target = v;
 				depth = v.depth;
 			}
 		}
 		
-		if(target != null)
-			touchManager.onTouchBegin(e.touch_id, e.x, e.y, target);
+		if (target != null)
+			touchManager.onTouchBegin(e.touch_id, helperVector.x, helperVector.y, target);
 	}
 	
 	function ontouchup(e:TouchEvent)
 	{
-		touchManager.onTouchEnd(e.touch_id, e.x, e.y);
+		var pos = translateTouchPos(e.pos, helperVector);
+		touchManager.onTouchEnd(e.touch_id, pos.x, pos.y);
 	}
 	
 	function ontouchmove(e:TouchEvent)
 	{
-		touchManager.onTouchMove(e.touch_id, e.x, e.y);
+		var pos = translateTouchPos(e.pos, helperVector);
+		touchManager.onTouchMove(e.touch_id, pos.x, pos.y);
+	}
+	
+	function translateTouchPos(inPos:Vector, ?outPos:Vector):Vector
+	{
+		if (outPos == null) outPos = new Vector();
+		outPos.x = inPos.x * Luxe.screen.width;
+		outPos.y = inPos.y * Luxe.screen.height;
+		return outPos;
 	}
 
 	
